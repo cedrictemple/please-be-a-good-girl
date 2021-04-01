cc944c9f9bce93dd8fbcfe729f943422cee624c8
Cedric Temple <cedrictemple@cedrictemple.info>
2021-04-01 13:51:55 +0200

    Add the credits scene connected to Menu ; get back to menu on close button in the credits scene closes #4

---
+++ CREDITS/CloseButton.gd
@@ -0,0 +1,10 @@
+extends Button
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	connect("pressed", self, "_button_pressed")
+
+func _button_pressed():
+	get_tree().change_scene("res://MENU.tscn")
+
