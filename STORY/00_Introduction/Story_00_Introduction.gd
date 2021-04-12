eed99a71be1c98a2377a8a990d207bbb88176ee3
Cédric Temple <cedrictemple@cedrictemple.info>
2021-04-12 17:50:34 +0200

    Text appears one char by one. It s better instead of before

--- STORY/00_Introduction/Story_00_Introduction.gd
+++ STORY/00_Introduction/Story_00_Introduction.gd
@@ -12,52 +12,55 @@ var text = ["Once upon a time, a little girl, was eating with his father.",
 			"[b]The father : [/b] Then ?",
 			"[b]The little Girl : [/b] Then, you know !",
 			"[b]The father : [/b] No, tell me.",
-			"[b]The little Girl : [/b] It's Green Monday ! I don't eat meat or fish.",
-			"[b]The father : [/b] Enough ! You bothered me",
-			"[b]The little Girl : [/b] I bothered you but I won't eat meat today.",
+			"[b]The little Girl : [/b] It's Green Monday ! I don't eat meat or fish on Green Monday.",
+			"[b]The father : [/b] Enough ! You're bothering me",
+			"[b]The little Girl : [/b] I'm bothering you but I won't eat meat today.",
 			"[b]The father : [/b] It's not the kind of thing you say to your father, you're a little girl, you will do what I say",
 			"[b]The little Girl : [/b] I'll never do what people wants if it's not what I want and, furthermore, if what they want is against me or my future",
-			"[b]The father : [/b] Enough is enough ! I punish you ! Go in the cave, thinking of what you have said !",
+			"[b]The father : [/b] Enough is enough ! You're punished ! Go in the cave, thinking of what you have said !",
 			"[b]The little Girl : [/b] Thanks !"]
 var textLines
 var appearingText
-var appearingTextPercent
+var appearingTextChars
+var textLineLength
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	textIndice = 0
-	appearingTextPercent = 0.0
+	appearingTextChars = 0
 	textLines = text.size()
 	appearingText = get_node("AppearingText")
-	appearingText.set_percent_visible(appearingTextPercent)
+	appearingText.set_visible_characters(appearingTextChars)
 	appearingText.bbcode_enabled = true
 	appearingText.bbcode_text = text[textIndice]
+	textLineLength = appearingText.text.length()
 	textIndice = textIndice+1
 	timer = Timer.new()
 	add_child(timer)
 	
-	timer.wait_time = 0.5
+	timer.wait_time = 0.05
 	timer.connect("timeout", self, "_timeout")
 	timer.start()
 
 
 func _timeout():
-	appearingTextPercent = appearingTextPercent + 0.1
-	if (appearingTextPercent < 1.0):
-		appearingText.set_percent_visible(appearingTextPercent)
-		timer.wait_time = 0.5
+	appearingTextChars = appearingTextChars + 1
+	if (appearingTextChars < textLineLength):
+		appearingText.set_visible_characters(appearingTextChars)
+		timer.wait_time = 0.05
 		timer.start()
-	elif (appearingTextPercent == 1.0):
-		appearingText.set_percent_visible(appearingTextPercent)
-		timer.wait_time = 1.0
+	elif (appearingTextChars == textLineLength):
+		appearingText.set_visible_characters(appearingTextChars)
+		timer.wait_time = 2.0
 		timer.start()
 	else:
-		appearingTextPercent = 0
+		appearingTextChars = 0
 		if (textIndice < textLines):
-			appearingText.set_percent_visible(appearingTextPercent)
+			appearingText.set_visible_characters(appearingTextChars)
 			appearingText.bbcode_text = text[textIndice]
 			textIndice = textIndice+1
-			timer.wait_time = 0.2
+			textLineLength = appearingText.text.length()
+			timer.wait_time = 0.05
 			timer.start()
 		else :
 			var status = get_tree().change_scene("res://LEVELS/LEVEL01/LEVEL01.tscn")
